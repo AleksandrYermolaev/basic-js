24a0e594cec1bc2a9e84146c6321aa82b54cc5d6
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-10-11 18:46:51 +0300

    feat: resolve 6 tasks

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -18,17 +18,13 @@ const HALF_LIFE_PERIOD = 5730;
  *
  */
 function dateSample(activity) {
-  console.log(activity);
   if (typeof(activity) !== 'string') return false;
   const activityNumber = +activity;
-  console.log(activityNumber);
   if (isNaN(activityNumber) || activityNumber <= 0) return false;
   if (activityNumber >= 15) return false;
   const lambda = 0.693 / HALF_LIFE_PERIOD;
   const activityPercent = activityNumber / MODERN_ACTIVITY;
-  console.log(activityPercent);
   const result = Math.ceil(- (Math.log(activityPercent) / lambda));
-  console.log(result);
   return result;
 }
 dateSample('3');
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,10 +13,75 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function transform(arr) {
+  try {
+    const voc = {
+	  '--discard-next': discardNext,
+	  '--double-next': doubleNext,
+      '--discard-prev': discardPrev,
+	  '--double-prev': doublePrev
+	}
+	function doubleNext(array, controlIndex) {
+	  if (controlIndex === array.length - 1) {
+	    return array.filter((value, index) => index < array.length - 1);
+	  }
+	  return array.map((value, index, array) => {
+        if (controlIndex === index) {
+	    	return value = array[index + 1];
+		} else {
+		  return value;
+		}
+	  });
+	} 
+	function doublePrev(array, controlIndex) {
+		if (controlIndex === 0) {
+		  return array.filter((value, index) => index > 0);
+		}
+		return array.map((value, index, array) => {
+		  if (controlIndex === index) {
+			  if (!array[index - 1]) {
+				delete value;
+			  } else {
+				return value = array[index - 1];
+			  }
+			  
+		  } else {
+			return value;
+		  }
+		});
+	} 
+	function discardNext(array, controlIndex) {
+		if (controlIndex === array.length - 1) {
+		  return array.filter((value, index) => index < array.length - 1);
+		}
+		const res = array.slice();
+		delete res[controlIndex];
+		delete res[controlIndex + 1];
+		return res;
+	} 
+	function discardPrev(array, controlIndex) {
+		if (controlIndex === 0) {
+		  return array.filter((value, index) => index > 0);
+		}
+		const res = array.slice();
+		res.splice(controlIndex - 1, 2);
+		return res;
+	} 
+	let result = arr;
+	for (let key in voc) {
+		for (let i = 0; i < result.length; i++) {
+		  if (key === result[i]) {
+	        result = voc[key](result, i);
+		  } 
+		}
+	}
+	
+	return result.filter(value => value);
+  } catch (err) {
+	throw new Error("'arr' parameter must be an instance of the Array!");
+  }
 }
+
 
 module.exports = {
   transform
