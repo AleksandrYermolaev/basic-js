14a8ec57fd791d7a935f403430db02606324058f
Aleksandr Yermolaev <sanyagt50@gmail.com>
2022-10-16 22:00:23 +0300

    feat: resolve some tasks

--- src/delete-digit.js
+++ src/delete-digit.js
@@ -11,11 +11,16 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For n = 152, the output should be 52
  *
  */
-function deleteDigit(/* n */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function deleteDigit(n) {
+  const arr = n.toString().split('');
+  let max = 0;
+  for (let i = 0; i < arr.length; i++) {
+	const current = +arr.slice(0, i).concat(arr.slice(i + 1)).join('');
+	if (current > max) max = current;
+  }
+  return max;
 }
-
+deleteDigit(152);
 module.exports = {
   deleteDigit
 };
--- src/encode-line.js
+++ src/encode-line.js
@@ -10,11 +10,28 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+	const arr = str.split('');
+	let res = [];
+	for (let i = 0; i < arr.length; i++) {
+		if (arr[i] === arr[i+1]) {
+			if (res[res.length - 1] === 2) {
+			  res = res.slice(0, res.length - 1);
+			  res.push(3);
+			} else if (res[res.length - 1] === 3) {
+			  res = res.slice(0, res.length - 1);
+			  res.push(4);
+			} else {
+				res.push(2);
+			}
+		} else {
+			res.push(arr[i]);
+		}
+	}
+	const result = res.join('');
+	return result;
 }
-
+encodeLine('');
 module.exports = {
   encodeLine
 };
